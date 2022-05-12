7fe6c271f9df7ff089469f9332d5ceaa74cd0f8f
darcdev <diegoandresrojas2000@gmail.com>
2022-05-12 14:01:13 -0500

    feat part8:library-backend update born date of an author

--- part-8/library-backend/index.js
+++ part-8/library-backend/index.js
@@ -110,6 +110,7 @@ const typeDefs = gql`
   }
   type Mutation {
     addBook(title:String!, author:String!,published:Int!,genres:[String!]!):Book!
+    editAuthor(name:String!, setBornTo:Int!):Author
   }
 `
 
@@ -140,6 +141,12 @@ const resolvers = {
       const book = { ...args, id: uuid() };
       books = books.concat(book);
       return book
+    },
+    editAuthor : (obj, args) => {
+      let author = authors.find(author => author.name === args.name);
+      if (!author) return null;
+      author.born = args.setBornTo;
+      return author
     }
   }
 }
