d7ebb27ff057cd33db817b4830743205a5f11ccb
darcdev <diegoandresrojas2000@gmail.com>
2022-05-13 00:19:30 -0500

    feat part8:library-backend add types and model user administration

--- part-8/library-backend/graphql/schema.js
+++ part-8/library-backend/graphql/schema.js
@@ -14,15 +14,27 @@ const typeDefs = gql`
     born:Int
     bookCount:Int!
   }
+  type User {
+    username: String!
+    favoriteGenre:String!
+    id:ID!
+  }
+  type Token {
+    value:String!
+  }
+
   type Query {
     bookCount: Int!
     authorsCount: Int!
     allBooks(author:String, genre:String):[Book!]!
     allAuthors:[Author!]!
+    me:User
   }
   type Mutation {
     addBook(title:String!, author:String!,published:Int!,genres:[String!]!):Book!
     editAuthor(name:String!, setBornTo:Int!):Author
+    createUser(username:String!,favoriteGenre:String!):User
+    login(username:String!,password:String!):Token
   }
 `
 module.exports = typeDefs
