8194a3cd4467aec4f79c99668bc6ed247eab21b8
darcdev <diegoandresrojas2000@gmail.com>
2022-05-29 00:26:31 -0500

    feat part9:body-mass-index add exercise calculator

---
+++ part-9/body-mass-index/exerciseCalculator.ts
@@ -0,0 +1,46 @@
+
+
+interface DiagnosticResult {
+    periodLength: number;
+    trainingDays: number;
+    success: boolean;
+    rating: number;
+    ratingDescription: string;
+    target: number;
+    average: number;
+}
+
+
+function calculateExercises(hoursByDay: Array<number>, target: number): DiagnosticResult {
+    
+    const periodLength = hoursByDay.length;
+    const average: number = hoursByDay.reduce((elem, acc) => acc + elem, 0) / periodLength;
+    const trainingDays: number = hoursByDay.filter(day => day > 0).length;
+    let success:boolean = false;
+    if (average >= target) success = true;
+    let rating: number;
+    let ratingDescription: string;
+    
+    if (average >= target) {
+        rating = 3;
+        ratingDescription = "Congratulations,You are awesome!!";
+    } else if (average > target * 0.50) {
+        rating = 2;
+        ratingDescription = "not too bad but could be better";
+    } else {
+        rating = 1
+        ratingDescription = "Not Good, But you can improve";
+    }
+
+    return {
+        periodLength,
+        trainingDays,
+        success,
+        rating,
+        ratingDescription,
+        target,
+        average
+    }
+}
+
+console.log(calculateExercises([3, 0, 2, 4.5,0,3,1], 2));
