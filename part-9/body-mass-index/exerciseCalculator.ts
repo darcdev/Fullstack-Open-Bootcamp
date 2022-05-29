fad5ad81f06caef3b31217dec3cc059265f41738
darcdev <diegoandresrojas2000@gmail.com>
2022-05-29 01:04:48 -0500

    feat part9:body-mass-index add calculator exercises to command line program

--- part-9/body-mass-index/exerciseCalculator.ts
+++ part-9/body-mass-index/exerciseCalculator.ts
@@ -43,4 +43,23 @@ function calculateExercises(hoursByDay: Array<number>, target: number): Diagnost
     }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5,0,3,1], 6));
+const args = process.argv.splice(2);
+
+try {
+
+    if (args.length < 2) {
+        throw new Error('Insufficient Arguments, Please Check your input');
+    }
+    const [targetArg, ...hoursByDayArg] :string[] = args ;
+    const target: number = Number(targetArg);
+    const hoursByDay: number[] = hoursByDayArg.map(hour => Number(hour));
+    console.log(calculateExercises(hoursByDay, target))
+    
+} catch (error: unknown) {
+      let errorMessage = 'Something went wrong.';
+      if (error instanceof Error) {
+            errorMessage += ` Error: ` + error.message;
+      }
+      console.log(errorMessage)
+}
+
