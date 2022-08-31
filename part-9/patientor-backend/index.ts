007b612580084be9cad05fea5c0410545b699948
darcdev <diegoandresrojas2000@gmail.com>
2022-08-30 21:43:45 -0500

    part-9:patientor-backend: add basic configuration

--- part-9/patientor-backend/index.ts
+++ part-9/patientor-backend/index.ts
@@ -3,14 +3,14 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.get('/api/ping', (_req, res) => {
     res.send('pong');
-})
+});
 
 const PORT = 3003;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
