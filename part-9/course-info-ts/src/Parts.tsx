af36272da1a0b1112d40b53b0d08def3d3b1325f
darcdev <diegoandresrojas2000@gmail.com>
2022-09-07 00:09:37 -0500

    feat part-9:course-info-ts add exhaustive type checking

--- part-9/course-info-ts/src/Parts.tsx
+++ part-9/course-info-ts/src/Parts.tsx
@@ -1,4 +1,5 @@
 import { ContentParams } from "./types";
+import assertNever from "./utils/assertNever";
 
 const Part = ({ courseParts }: ContentParams) => {
 
@@ -13,7 +14,8 @@ const Part = ({ courseParts }: ContentParams) => {
                     return <div><h3>{part.name} {part.exerciseCount}</h3><p>{part.description}</p><p>submit to {part.exerciseSubmissionLink} </p></div>;
                 case "special":
                     return <div><h3>{part.name} {part.exerciseCount}</h3><p>{part.description}</p><p>required skills: {part.requirements.join(",")} </p></div>;
-
+                default:
+                    return assertNever(part);
             }
         })}
     </div>
