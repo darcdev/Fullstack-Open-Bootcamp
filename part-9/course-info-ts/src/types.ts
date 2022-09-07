7c104e778aedfd56482cec15dde0033f446b772d
darcdev <diegoandresrojas2000@gmail.com>
2022-09-06 22:38:16 -0500

    feat part-9:course-info-ts create description interface and expands with CoursePartBase

--- part-9/course-info-ts/src/types.ts
+++ part-9/course-info-ts/src/types.ts
@@ -2,10 +2,32 @@ export interface HomeParams {
     title: string
 }
 
-export interface Course {
-    name: string,
-    exerciseCount: number
+interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+    type: string;
 }
+
+interface CoursePartDescription extends CoursePartBase {
+    description: string;
+}
+
+interface CourseNormalPart extends CoursePartDescription {
+    type: "normal";
+}
+
+interface CourseProjectPart extends CoursePartBase {
+    type: "groupProject";
+    groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartDescription {
+    type: "submission";
+    exerciseSubmissionLink: string;
+}
+
+export type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart
+
 export interface ContentParams {
-    courseParts: Course[]
+    courseParts: CoursePart[]
 }
